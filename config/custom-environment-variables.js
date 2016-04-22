c10fac68dddff8d524ca16914732a5a379f3b48f
shirls81 <blond-shirls@hotmail.com>
2016-04-21 19:18:13 -0600

    final final edit

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,6 +1,5 @@
 'use strict'
 module.exports = {
   port: 'PORT',
-  mongoUrl: 'MONGO_URL',
-  mongodb://heroku_z4k0tdb8:4voin0jfaopir9hhsitk32qlo4@ds013991.mlab.com:13991/heroku_z4k0tdb8
+  mongoUrl: 'MONGO_URL'
 }
